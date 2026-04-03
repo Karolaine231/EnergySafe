ea00293996d83efa3af89059d77120c82cb1029d
Karolaine Helena Santos Silva <153235224+Karolaine231@users.noreply.github.com>
2026-04-03 10:06:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-
 from database import engine
 import models
 from routes import medicoes, alertas, locais, quadros, dispositivos, canais
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     models.Base.metadata.create_all(bind=engine)
     yield
-
 
 app = FastAPI(
     title="EnergySafe API",
@@ -22,7 +19,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://energy-safe-9m2q.vercel.app/"],  # troque pelo domínio do Vercel em produção ex: ["https://seu-projeto.vercel.app"]
+    allow_origins=[
+        "https://energy-safe.vercel.app",        # ← seu site principal
+        "https://energy-safe-9m2q.vercel.app",   # ← domínio alternativo Vercel
+        "http://localhost:5500",                  # ← teste local VS Code
+        "http://127.0.0.1:5500",
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -33,7 +37,6 @@ app.include_router(dispositivos.router)
 app.include_router(canais.router)
 app.include_router(medicoes.router)
 app.include_router(alertas.router)
-
 
 @app.get("/", tags=["Health"])
 def health_check():
